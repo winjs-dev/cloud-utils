637b4d395f0984695f3a9b7a105b103fda1a69f7
cklwblove <lwbhtml@163.com>
2022-08-18 11:17:29 +0800

    fix: 🐛 调整发布脚本

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -7,4 +7,3 @@ git init
 git add -A
 git commit -m 'update docs'
 git push -f git@github.com:cloud-templates/cloud-utils.git master:gh-pages
-git push -f git@gitee.com:cklwblove/cloud-utils.git master:gh-pages
